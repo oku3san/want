3ef84ce4e9d6130982d39d4d158500210d18120b
oku3san <takaya0303@gmail.com>
2023-03-04 23:36:49 +0900

    Update

--- cmd/root.go
+++ cmd/root.go
@@ -32,13 +32,14 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-    // Uncomment the following line if your bare application
-    // has an action associated with it:
-    // Run: func(cmd *cobra.Command, args []string) { },
     Run: func(cmd *cobra.Command, args []string) {
+        // URL を変数でもつ
         endpoint := "https://api.openai.com/v1/chat/completions"
+
+        // API キー取得
         apiKey := os.Getenv("OPENAI_API_KEY")
 
+        // chatgpt に投げるメッセージを作成
         messages = append(messages, Message{
             Role:    "user",
             Content: "who is batman?",
@@ -86,8 +87,6 @@ to quickly create a Cobra application.`,
     },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
     err := rootCmd.Execute()
     if err != nil {
@@ -96,13 +95,5 @@ func Execute() {
 }
 
 func init() {
-    // Here you will define your flags and configuration settings.
-    // Cobra supports persistent flags, which, if defined here,
-    // will be global for your application.
-
-    // rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.want.yaml)")
-
-    // Cobra also supports local flags, which will only run
-    // when this action is called directly.
     rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
